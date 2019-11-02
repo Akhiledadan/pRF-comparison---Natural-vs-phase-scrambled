2abc48abe0bbf0a14a5261ec296d9c1967e63d2d
akhil <akhilddn5@gmail.com>
2019-11-02 10:34:51 +0100

    Remove duplicated directory

--- Analysis/pRFProp_comparison/NP_loadPaths_all.m
+++
@@ -1,9 +0,0 @@
-function dirPth = NP_loadPaths_all
-
-%% ----- folder to save results (figures, data) ------
-
-dirPth.saveDirFig     = fullfile(NP_rootPath,'data','All','plots');
-dirPth.saveDirRes     = fullfile(NP_rootPath,'data','All','results');
-dirPth.saveDirMSFig     = fullfile(NP_rootPath,'data','All','MSFigs');
-
-dirPth.saveDirSup3 = '/mnt/storage_2/projects/Nat_PhScr/MS/finalFig/supplementary/S_3/';
